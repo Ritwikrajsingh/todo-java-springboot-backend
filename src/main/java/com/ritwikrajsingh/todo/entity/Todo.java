c448279af8fbd593ee62e39c41b8f2be571fda19
ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-09-08 07:12:28 +0530

    added Builder annotation to Todo entity for object creation using builder pattern

--- src/main/java/com/ritwikrajsingh/todo/entity/Todo.java
+++ src/main/java/com/ritwikrajsingh/todo/entity/Todo.java
@@ -3,10 +3,12 @@ package com.ritwikrajsingh.todo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Data
+@Builder
 public class Todo {
     @Id
     @GeneratedValue
