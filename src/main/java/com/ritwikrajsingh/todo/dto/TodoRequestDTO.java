11b2aa0d57ea7b15122932e261b45e4da5b238a9
ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-09-09 07:14:22 +0530

    refactored TodoRequestDTO: added toEntity method for easier conversion to Todo entity

--- src/main/java/com/ritwikrajsingh/todo/dto/TodoRequestDTO.java
+++ src/main/java/com/ritwikrajsingh/todo/dto/TodoRequestDTO.java
@@ -1,9 +1,15 @@
 package com.ritwikrajsingh.todo.dto;
 
+import com.ritwikrajsingh.todo.entity.Todo;
+
 import lombok.Data;
 
 @Data
 public class TodoRequestDTO {
-    private String title;
+    private String task;
     private boolean isComplete;
+
+    public Todo toEntity() {
+        return Todo.builder().task(this.task).isComplete(this.isComplete).build();
+    }
 }
