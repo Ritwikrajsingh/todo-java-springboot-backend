5b80871b4d3ca8d7ce88ee29048dc11dbf8db741
ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-09-15 08:26:09 +0530

    add TodoController for mapping api routes with CRUD operations

---
+++ src/main/java/com/ritwikrajsingh/todo/controller/TodoController.java
@@ -0,0 +1,58 @@
+package com.ritwikrajsingh.todo.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ritwikrajsingh.todo.dto.TodoPatchRequestDTO;
+import com.ritwikrajsingh.todo.dto.TodoRequestDTO;
+import com.ritwikrajsingh.todo.dto.TodoResponseDTO;
+import com.ritwikrajsingh.todo.service.impl.TodoServiceImpl;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/todos")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+public class TodoController {
+    private final TodoServiceImpl service;
+
+    @PostMapping
+    public ResponseEntity<TodoResponseDTO> createTodo(@RequestBody TodoRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.createTodo(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TodoResponseDTO>> getAllTodos(@RequestParam String param) {
+        return ResponseEntity.ok(this.service.getAllTodos());
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<TodoResponseDTO> updateTodoById(@PathVariable Integer id, @RequestBody TodoRequestDTO dto) {
+        return ResponseEntity.ok(this.service.updateTodoById(id, dto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TodoResponseDTO> patchTodoById(@PathVariable Integer id,
+            @RequestBody TodoPatchRequestDTO dto) {
+        return ResponseEntity.ok(this.service.patchTodoById(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTodoById(@PathVariable Integer id) {
+        this.service.deleteTodoById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
