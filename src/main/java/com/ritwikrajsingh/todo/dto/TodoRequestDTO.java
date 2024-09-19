2be65a56163797d51927c1c5318b028c7edfbe06
ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-09-19 08:50:40 +0530

    fix: added validation annotations to TodoRequestDTO for task field

--- src/main/java/com/ritwikrajsingh/todo/dto/TodoRequestDTO.java
+++ src/main/java/com/ritwikrajsingh/todo/dto/TodoRequestDTO.java
@@ -2,10 +2,14 @@ package com.ritwikrajsingh.todo.dto;
 
 import com.ritwikrajsingh.todo.entity.Todo;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TodoRequestDTO {
+    @NotNull(message = "Task cannot be null.")
+    @NotEmpty(message = "Task cannot be empty.")
     private String task;
     private boolean isComplete;
 
