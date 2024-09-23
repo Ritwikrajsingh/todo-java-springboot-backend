ecafb859e9984b212c5a95ae5e354856c6d9117b
ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-09-23 09:37:13 +0530

    added ErrorMessageDTO and implemented it for standardized error responses in GlobalExceptionHandler

---
+++ src/main/java/com/ritwikrajsingh/todo/dto/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package com.ritwikrajsingh.todo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+    private String message;
+
+}
--- src/main/java/com/ritwikrajsingh/todo/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ritwikrajsingh/todo/exception/handler/GlobalExceptionHandler.java
@@ -7,36 +7,44 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.ritwikrajsingh.todo.dto.ErrorMessageDTO;
+
 import jakarta.persistence.EntityNotFoundException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> entityNotFoundExceptionHandler(EntityNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<ErrorMessageDTO> entityNotFoundExceptionHandler(EntityNotFoundException ex) {
+        ErrorMessageDTO error = new ErrorMessageDTO(ex.getMessage());
+        System.out.println("this is a test: " + error);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<ErrorMessageDTO> illegalArgumentExceptionHandler(IllegalArgumentException ex) {
+        ErrorMessageDTO error = new ErrorMessageDTO(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<ErrorMessageDTO> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
+        ErrorMessageDTO error = new ErrorMessageDTO(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<String> httpRequestMethodNotSupportedExceptionHandler(
+    public ResponseEntity<ErrorMessageDTO> httpRequestMethodNotSupportedExceptionHandler(
             HttpRequestMethodNotSupportedException ex) {
-        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(ex.getMessage());
+        ErrorMessageDTO error = new ErrorMessageDTO(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(error);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> defaultHandler(Exception ex) {
+    public ResponseEntity<ErrorMessageDTO> defaultHandler(Exception ex) {
+        ErrorMessageDTO error = new ErrorMessageDTO("An unexpected error occurred: " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("An unexpected error occurred: " + ex.getMessage());
+                .body(error);
     }
 
 }
